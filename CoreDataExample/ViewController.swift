ca6118724c8d488c2d3d7944f9717ee89c052302
Vitaliy Smola <smola.vetal@gmail.com>
2019-05-20 18:50:51 +0300

    v1.0

--- CoreDataExample/ViewController.swift
+++ CoreDataExample/ViewController.swift
@@ -7,19 +7,102 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    var users = [Persone]()
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
     }
-
-
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdent") as! itemCell
+        let user = users[indexPath.row]
+        cell.Name.text = user.name
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Change user", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Change", style: .default) {(action) in
+            self.users[indexPath.row].setValue(textField.text, forKey: "Name")
+            self.saveData()
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTF) in
+            textField = alertTF
+            textField.placeholder = "New user"
+        }
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        switch editingStyle {
+        case .delete:
+            context.delete(users[indexPath.row])
+            users.remove(at: indexPath.row)
+            saveData()
+        default:
+            return
+        }
+    }
+    
+    func saveData() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving \(error)")
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    func loadData() {
+        let request: NSFetchRequest<Persone> = Persone.fetchRequest()
+        
+        do {
+            users = try context.fetch(request)
+        } catch {
+            print("Error \(error)")
+        }
+        
+        DispatchQueue.main.async {
+            self.tableView.reloadData()
+        }
+    }
+    
+    @IBAction func createUser(_ sender: Any) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "New user", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add", style: .default) {(action) in
+            let newUser = Persone(context: self.context)
+            newUser.name = textField.text
+            self.users.append(newUser)
+            self.saveData()
+        }
+        alert.addAction(action)
+        alert.addTextField { (alertTF) in
+            textField = alertTF
+            textField.placeholder = "New user"
+        }
+        present(alert, animated: true, completion: nil)
+    }
 }
 
---
+++ CoreDataExample/itemCell.swift
@@ -0,0 +1,13 @@
+//
+//  itemCell.swift
+//  CoreDataExample
+//
+//  Created by Vitaliy on 8/24/18.
+//  Copyright © 2018 Vitaliy. All rights reserved.
+//
+
+import UIKit
+
+class itemCell: UITableViewCell {
+    @IBOutlet weak var Name: UILabel!
+}
